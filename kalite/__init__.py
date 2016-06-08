24f987737b80c54985f19584bbcfbea0acf59eab
Mario Sanchez Prada <mario@endlessm.com>
2016-06-08 15:36:35 +0100

    Make ROOT_DATA_PATH consider the KALITE_DIR environment variable
    This will prevent `kalite start` from failing to find the static media files
    that it will try to copy on the first run if the system prefix is not the
    standard one, which would cause assets to be unresolvable when loading.
    
    https://github.com/learningequality/ka-lite/issues/5143

--- kalite/__init__.py
+++ kalite/__init__.py
@@ -7,4 +7,11 @@ from version import *
 __version__ = VERSION
 
 # ROOT_DATA_PATH is *not* where the source files live. It's a place where non-user-writable data files may be written.
-ROOT_DATA_PATH = os.path.join(sys.prefix, 'share', 'kalite')
+ROOT_DATA_PATH = None
+
+# Consider KALITE_DIR here or we will keep pointing to the sys.prefix (e.g. /usr)
+# even in envorinments when it does not make any sense (e.g. flatpak bundles).
+if 'KALITE_DIR' in os.environ:
+    ROOT_DATA_PATH = os.environ['KALITE_DIR']
+else:
+    ROOT_DATA_PATH = os.path.join(sys.prefix, 'share', 'kalite')
